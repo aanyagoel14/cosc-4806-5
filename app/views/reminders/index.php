792e9960006564c2235efbe2794752885c015b55
Aanya Goel <131501514+aanyagoel14@users.noreply.github.com>
2025-07-13 23:58:26 +0000

    Updating Public Header in headerPublic.php

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -3,6 +3,16 @@
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page"><?= ucwords($_SESSION['controller']);?></li>
+                    </ol>    
+                </nav>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
                 <h1>Reminders</h1>
                 <p><a href="/reminders/create" class="btn btn-primary">Create a new reminder</a></p>
             </div>
